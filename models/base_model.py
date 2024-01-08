9a1c9a06111f97bc190c858be0c3a74aee52422b
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-08 09:53:25 +0100

    create the Basemodel class

---
+++ models/base_model.py
@@ -0,0 +1,17 @@
+import uuid
+import datetime
+
+class BaseModel():
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dic(self):
+        
