fe644eb5ead4fe69e2f437f5b627c5e1d1d41e75
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-08 11:09:23 +0100

    workign in task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class FileStorage():
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        pass
+    def new(self):
+        pass
+    def save(self):
+        pass
+    def reload(self):
+        pass
