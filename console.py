5bcde1b83b8ff284f5de31e8764105d64749059d
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-08 12:03:01 +0100

    workign in task 6

--- console.py
+++ console.py
@@ -1,5 +1,12 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        return True
     
-    pass
+    def do_EOF(self, arg):
+        print()
+        return True
