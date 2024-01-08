84faaaeb5707c6a90988da73f99d24311bc1aa99
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-08 11:17:56 +0100

    workign in task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
+import json
+
 class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         return FileStorage.__objects
-    
+
     def new(self, obj):
-        
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
     def save(self):
-        pass
+        serialized_objects = {}
+        for key, value in FileStorage.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+        
     def reload(self):
         pass
