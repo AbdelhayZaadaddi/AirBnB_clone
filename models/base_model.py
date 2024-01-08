0f7a4fea2425fd42eb25f4b4a9b441a9e0057c4a
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-08 10:58:27 +0100

    workign in task 4 test done

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,12 @@ class BaseModel():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
                         value = datetime.strptime(value, timeformt)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+                    setattr(self, key, value)
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
