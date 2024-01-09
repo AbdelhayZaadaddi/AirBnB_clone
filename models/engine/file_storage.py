d3854b3299f6c4d6d26e0b5cb205fd09decab6bc
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-09 10:55:41 +0100

    working on task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,7 +45,7 @@ class FileStorage:
                         from models.city import City
                         from models.amenity import Amenity
                         from models.place import Place
-                        
+                        from models.review import Review
 
                         obj = eval(key.split(".")[0])(**value)
                         FileStorage.__objects[key] = obj
