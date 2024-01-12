5eaa66c06406d8705b71972de864f3bde542f3f1
HudaOmer <hudaoe175@gmail.com>
2024-01-12 06:41:08 -0800

    Add tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,9 +2,21 @@
 """Unit test for Base Model"""
 
 import unittest
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     """class for unit test -> BaseModel"""
 
-    pass
+    def SetUp(self):
+        model_for_test = BaseModel()
+
+    def TearDown(self):
+        pass
+
+    def test_str(self):
+        self.assertequal(model_for_test.__str__(),
+                          f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+if __name__ == "__main__":
+    unittest.main()
