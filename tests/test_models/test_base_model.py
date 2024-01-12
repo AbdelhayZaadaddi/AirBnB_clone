a0dce48d1b896eaca25ffaa638397ecafc8a8d8b
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-13 01:32:28 +0300

    Test for id

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,17 +10,21 @@ class TestBaseModel(unittest.TestCase):
     """class for unit test -> BaseModel"""
 
     def setUp(self):
-        print("setup") 
+        #print("setup") 
         self.model = BaseModel()
+        self.model2 = BaseModel()
 
     def tearDown(self):
-        print("tear down")
+        pass #print("tear down")
 
     def test_str(self):
         #model = BaseModel()
         self.assertEqual(8,4+4)
         self.assertEqual(self.model.__str__(),
                          f"[{self.model.__class__.__name__}] ({self.model.id}) {self.model.__dict__}")
+
+    def test_uuid(self):
+         self.assertNotEqual(self.model.id, self.model2.id)
 
 if __name__ == "__main__":
     unittest.main()
