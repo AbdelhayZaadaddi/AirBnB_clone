63f514f74db82a73b747260ccf84be74297cb578
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-12 10:11:57 +0100

    fixed consol.pu

--- console.py
+++ console.py
@@ -4,6 +4,11 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import Satate
+from models.place import Place
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
 from models import storage
 
 
