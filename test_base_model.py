8e2ce4f62b22d8c5183e9f11b9b947f60126a838
HudaOmer <hudaoe175@gmail.com>
2024-01-12 14:09:13 -0800

    Add tests

--- test_base_model.py
+++ test_base_model.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+"""Unit test for Base Model"""
+
+import unittest
+import os
 from models.base_model import BaseModel
 
+print(os.getcwd())
+class TestBaseModel(unittest.TestCase):
+    """class for unit test -> BaseModel"""
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-          my_model_json[key]))
+    def setUp(self):
+        print("setup") 
+        self.model = BaseModel()
+
+    def tearDown(self):
+        print("tear down")
+
+    def test_str(self):
+        #model = BaseModel()
+        self.assertEqual(8,4+4)
+        self.assertEqual(self.model.__str__(),
+                         f"[{self.model.__class__.__name__}] ({self.model.id}) {self.model.__dict__}")
+
+if __name__ == "__main__":
+    unittest.main()
