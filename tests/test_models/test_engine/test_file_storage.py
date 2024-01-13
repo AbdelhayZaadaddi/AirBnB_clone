b79b652464ccbc742f165612e1bc29b54e33a553
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-13 14:05:27 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,22 @@
-import unittest 
+#!/usr/bin/python3
+"""Unit test for File Storage"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """class for unit test -> File Storage"""
+
+    def setUp(self):
+        """Set Up the two models"""
+        self.model = BaseModel()
+        self.model2 = BaseModel()
+
+    def tearDown(self):
+        """Tear down function"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
