81093608015af5844712053e657fd75191b78794
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-13 13:13:37 +0100

    plpl

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-'''
-FileStorage module Containig the FileStorage class
-'''
+#!/usr/bin/python3
+
+"""This module implements the FileStorage class"""
 
 import json
+from datetime import datetime
 
 
-class FileStorage:
+class FileStorage():
     '''FileStorage class'''
     __file_path = "file.json"
     __objects = {}
@@ -17,8 +17,9 @@ class FileStorage:
 
     def new(self, obj):
         """Adds a new object to the dictionary"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes objects and saves them to a JSON file"""
@@ -34,29 +35,27 @@ class FileStorage:
         """Deserializes objects from a JSON file
         and loads them into the dictionary"""
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
+            with open(FileStorage.__file_path, 'r') as f:
+                dict_obj = json.load(f)
 
-                for key, value in data.items():
+                for key, value in dict_obj.items():
                     class_name = value["__class__"]
-
                     if class_name in globals():
-                        class_obj = globals()[class_name]
-                        print(class_obj)
+                        obj_class = globals()[class_name]
+                        print(obj_class)
                         del value["__class__"]
-                        obj_instance = class_obj(**value)
-                        FileStorage.__objects[key] = obj_instance
+                        obj = obj_class(**value)
+                        FileStorage.__objects[key] = obj
                     else:
                         from models.base_model import BaseModel
-                        from models.user import User
-                        from models.state import Satate
                         from models.city import City
-                        from models.amenity import Amenity
+                        from models.user import User
                         from models.place import Place
                         from models.review import Review
+                        from models.state import State
+                        from models.amenity import Amenity
 
                         obj = eval(key.split(".")[0])(**value)
                         FileStorage.__objects[key] = obj
-
         except FileNotFoundError:
             return
