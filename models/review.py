ad3a82df01acd25881077161a3454a624dcc8659
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-13 17:02:14 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''Class Review that inherits from the Basemodel'''
+"""Class Review that inherits from the Basemodel"""
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    '''Review Class'''
+    """Review Class"""
     place_id = ""
     user_id = ""
     text = ""
