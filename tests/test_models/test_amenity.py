b33a6268d6ad8cc5947a38c0a519c24cfaab5b10
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-13 14:01:59 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,4 +1,23 @@
-import unittest
+#!/usr/bin/python3
+"""Unit test for Amenity"""
 
-class Test():
-    pass
+import unittest
+import os
+from models.base_model import BaseModel
+
+
+class TestAmenity(unittest.TestCase):
+    """class for unit test -> Amenity"""
+
+    def setUp(self):
+        """Set Up the two models"""
+        self.model = BaseModel()
+        self.model2 = BaseModel()
+
+    def tearDown(self):
+        """Tear down function"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
