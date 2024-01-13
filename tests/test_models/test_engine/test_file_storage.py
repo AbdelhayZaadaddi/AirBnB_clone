a8fd3fde3da580d93562fc34f531b2452bcb33c0
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-13 13:50:03 +0100

    fixed?

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,22 +1,99 @@
 #!/usr/bin/python3
-"""Unit test for File Storage"""
+"""Unit tests for the FileStorage class"""
 
 import unittest
+import os
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    """class for unit test -> File Storage"""
+    """Test cases for the FileStorage class"""
 
     def setUp(self):
-        """Set Up the two models"""
+        """Set up testing environment"""
+        self.storage = FileStorage()
         self.model = BaseModel()
-        self.model2 = BaseModel()
 
-    def tearDown(self):
-        """Tear down function"""
-        pass
+    def test_all(self):
+        """Test all method of FileStorage"""
+        all_objects = self.storage.all()
+        self.assertIsInstance(all_objects, dict)
+
+    def test_new(self):
+        """Test new method of FileStorage"""
+        self.storage.new(self.model)
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.assertIn(key, self.storage.all())
+
+    def test_save(self):
+        """Test save method of FileStorage"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_reload(self):
+        """Test reload method of FileStorage"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.storage.reload()
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.assertIn(key, self.storage.all())
+
+    def test_new_with_none(self):
+        """Test new method with None as argument"""
+        self.storage.new(None)
+        self.assertEqual(len(self.storage.all()), 0)
+
+    def test_save_with_no_new_objects(self):
+        """Test save method with no new objects"""
+        self.storage.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_reload_with_no_file(self):
+        """Test reload method with no file"""
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        self.storage.reload()
+        self.assertEqual(len(self.storage.all()), 0)
+
+    def test_reload_with_file(self):
+        """Test reload method with file"""
+        self.storage.new(self.model)
+        self.storage.save()
+        self.storage.__objects = {}
+        self.storage.reload()
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.assertIn(key, self.storage.all())
+
+    def test_new_with_existing_key(self):
+        """Test new method with an existing key"""
+        key = "{}.{}".format(type(self.model).__name__, self.model.id)
+        self.storage.new(self.model)
+        self.assertIn(key, self.storage.all())
+        old_object = self.storage.all()[key]
+        self.storage.new(self.model)
+        new_object = self.storage.all()[key]
+        self.assertEqual(old_object, new_object)
+
+    def test_save_with_existing_file(self):
+        """Test save method with an existing file"""
+        self.storage.new(self.model)
+        self.storage.save()
+        old_modification_time = os.path.getmtime('file.json')
+        self.storage.save()
+        new_modification_time = os.path.getmtime('file.json')
+        self.assertNotEqual(old_modification_time, new_modification_time)
+
+    def test_reload_with_existing_objects(self):
+        """Test reload method with existing objects"""
+        self.storage.new(self.model)
+        self.storage.save()
+        old_objects = self.storage.all()
+        self.storage.reload()
+        new_objects = self.storage.all()
+        self.assertEqual(old_objects, new_objects)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
