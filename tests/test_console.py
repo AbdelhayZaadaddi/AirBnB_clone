65693e49237a3f0a5e762bd2548fe5724d16954a
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-13 14:14:52 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,8 +2,22 @@
 """Unit test for Console"""
 
 import unittest
+from models.base_model import BaseModel
 
 
 class TestConsole(unittest.TestCase):
     """Unit Test for Console"""
-    pass
+
+
+    def setUp(self):
+        """Set Up the two models"""
+        self.model = BaseModel()
+        self.model2 = BaseModel()
+
+    def tearDown(self):
+        """Tear down function"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
